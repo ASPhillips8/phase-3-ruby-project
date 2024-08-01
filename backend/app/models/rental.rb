0cd7da9f18afac8a6b6e89847f8241c691853e30
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-08-01 10:51:49 -0400

    Create avereage rental length

--- backend/app/models/rental.rb
+++ backend/app/models/rental.rb
@@ -15,4 +15,18 @@ class Rental < ActiveRecord::Base
   def customer_names
     customer.full_name
   end
+
+  def self.open_rentals
+    where(date_in: nil).count
+  end
+
+  def self.most_popular_tool
+    joins(:tool).group("tools.id").order("COUNT(rentals.id) DESC").limit(1).pluck("tools.name").first
+  end
+
+  def self.average_rental_length
+    completed_rentals = where.not(date_in: nil)
+    total_days = completed_rentals.sum(&:rental_time)
+    completed_rentals.any? ? (total_days / completed_rentals.size.to_f) : 0.0
+  end
 end
