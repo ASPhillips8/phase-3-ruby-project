d5755fa6c9ac87a05cda79ffd6749e073d26b938
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-08-01 14:25:15 -0400

    Updated NavBar style

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,22 +1,41 @@
 import React from "react"
 import { NavLink } from "react-router-dom"
-// import "./NavBar.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./NavBar.css"
 
 function NavBar() {
   return (
-    <nav className="nav">
-      <NavLink to="/tools" className="navbar">
-        Tools
-      </NavLink>
-      <NavLink to="/customers" className="navbar">
-        Customers
-      </NavLink>
-      <NavLink to="/rentals" className="navbar">
-        Rentals
-      </NavLink>
-      <NavLink to="/stats" className="navbar">
-        Stats
-      </NavLink>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-center">
+      <div className="navbar-nav">
+        <NavLink
+          to="/tools"
+          className="nav-link btn btn-outline-primary rounded-pill mx-2"
+          activeClassName="active"
+        >
+          Tools
+        </NavLink>
+        <NavLink
+          to="/customers"
+          className="nav-link btn btn-outline-primary rounded-pill mx-2"
+          activeClassName="active"
+        >
+          Customers
+        </NavLink>
+        <NavLink
+          to="/rentals"
+          className="nav-link btn btn-outline-primary rounded-pill mx-2"
+          activeClassName="active"
+        >
+          Rentals
+        </NavLink>
+        <NavLink
+          to="/stats"
+          className="nav-link btn btn-outline-primary rounded-pill mx-2"
+          activeClassName="active"
+        >
+          Stats
+        </NavLink>
+      </div>
     </nav>
   )
 }
