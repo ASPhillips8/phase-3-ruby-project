6d6125f3693d066be30c64591413fe89972c58d9
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-08-01 11:15:46 -0400

    Create route and Stat page

---
+++ frontend/src/pages/Stats.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react"
+
+const Stats = () => {
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <div>
+        <h2>Customer Stats</h2>
+      </div>
+      <div>
+        <h2>Rental Stats</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Stats
