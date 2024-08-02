9bc7fe58faae991f7174a968e988fa7641ca3334
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-08-02 11:15:54 -0400

    Update schema

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_07_29_165943) do
-
+ActiveRecord::Schema.define(version: 20_240_729_165_943) do
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
