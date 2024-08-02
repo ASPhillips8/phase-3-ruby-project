8efe517a7ca0d48ec06cb5d40a0d690c40b71ddd
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-08-02 00:19:44 -0400

    Updated Header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,14 +1,20 @@
 import React from "react"
+import "bootstrap/dist/css/bootstrap.min.css" // Make sure Bootstrap is imported
 
 function Header() {
   return (
-    <header className="header">
-      <h1>
-        Lumpy Tool Rental
-        <span className="logo" role="img">
-          🛠️
-        </span>
-      </h1>
+    <header className="header bg-info py-3">
+      <div className="container d-flex align-items-center">
+        <h1
+          className="text-white"
+          style={{ fontFamily: "'Comic Sans MS', cursive, sans-serif" }}
+        >
+          Lumpy Tool Rental
+          <span className="logo" role="img">
+            🛠️
+          </span>
+        </h1>
+      </div>
     </header>
   )
 }
